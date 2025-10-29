320603b5ef41c493733bcf8676ac7db9df019051
Ryan Carlos <ryancarlosferreira1849@gmail.com>
2025-10-29 13:28:52 -0300

    add page informations

--- frontend/src/app/vaccines/infantis/page.tsx
+++ frontend/src/app/vaccines/infantis/page.tsx
@@ -1,5 +1,111 @@
-
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Circle } from "lucide-react";
 
 export default function Infantis() {
-    return <div></div>
+  return (
+    <main>
+      <section className="grid grid-cols-2 lg:grid-cols-4 gap-4">
+        <Card>
+          <CardHeader>
+            <div className="flex items-center">
+              <CardTitle className="text-lg sm:text-lg text-gray-600 select-none">
+                BCG
+              </CardTitle>
+            </div>
+
+            <CardDescription>
+              <div className="inline-flex items-center justify-center gap-2">
+                <Circle className="bg-green-600 ml-auto w-4 h-4 rounded-full" />
+                <h1>Em dia</h1>
+              </div>
+            </CardDescription>
+          </CardHeader>
+
+          <CardContent>
+            <Button variant="outline" size="sm">
+              Ver Detalhes
+            </Button>
+          </CardContent>
+        </Card>
+
+        <Card>
+          <CardHeader>
+            <div className="flex items-center">
+              <CardTitle className="text-lg sm:text-lg text-gray-600 select-none">
+                Hepatite B
+              </CardTitle>
+            </div>
+
+             <CardDescription>
+              <div className="inline-flex items-center justify-center gap-2">
+                <Circle className="bg-green-600 ml-auto w-4 h-4 rounded-full" />
+                <h1>Em dia</h1>
+              </div>
+            </CardDescription>
+          </CardHeader>
+
+          <CardContent>
+            <Button variant="outline" size="sm">
+              Ver Detalhes
+            </Button>
+          </CardContent>
+        </Card>
+
+        <Card>
+          <CardHeader>
+            <div className="flex items-center">
+              <CardTitle className="text-lg sm:text-lg text-gray-600 select-none">
+                Pentavalente
+              </CardTitle>
+            </div>
+
+             <CardDescription>
+              <div className="inline-flex items-center justify-center gap-2">
+                <Circle className="bg-green-600 ml-auto w-4 h-4 rounded-full" />
+                <h1>Em dia</h1>
+              </div>
+            </CardDescription>
+          </CardHeader>
+
+          <CardContent>
+            <Button variant="outline" size="sm">
+              Ver Detalhes
+            </Button>
+          </CardContent>
+        </Card>
+
+        <Card>
+          <CardHeader>
+            <div className="flex items-center">
+              <CardTitle className="text-lg sm:text-lg text-gray-600 select-none">
+                VIP - Poliomielite
+              </CardTitle>
+            </div>
+
+             <CardDescription>
+              <div className="inline-flex items-center justify-center gap-2">
+                <Circle className="bg-red-600 ml-auto w-4 h-4 rounded-full" />
+                <h1>Pendente</h1>
+              </div>
+            </CardDescription>
+          </CardHeader>
+
+          <CardContent>
+            <Button variant="outline" size="sm">
+              Ver Detalhes
+            </Button>
+          </CardContent>
+        </Card>
+
+        
+      </section>
+    </main>
+  );
 }
