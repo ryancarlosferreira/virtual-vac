a11b4f78219d059cf0626fa07b8f6e7f28e34e4b
Ryan Carlos <ryancarlosferreira1849@gmail.com>
2025-11-09 17:14:07 -0300

    adjusting admin vaccine page

--- frontend/src/app/_components/admin-form.tsx
+++ frontend/src/app/_components/admin-form.tsx
@@ -3,6 +3,7 @@
 import { Loader } from "lucide-react";
 import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import {
   Select,
@@ -12,11 +13,12 @@ import {
   SelectValue,
 } from "@/components/ui/select";
 
-import { useForm } from "react-hook-form";
+import { useForm, Controller } from "react-hook-form";
 
 import toast from "react-hot-toast";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { ErrorMessage } from "@hookform/error-message";
+import { useHookFormMask } from "use-mask-input";
 
 import { AdminData, adminSchema } from "@/app/_validators/admin-validator";
 import api from "@/axios/api";
@@ -26,14 +28,29 @@ export default function AdminForm() {
   const {
     handleSubmit,
     register,
+    control,
     formState: { isSubmitting, errors },
   } = useForm<AdminData>({
     resolver: zodResolver(adminSchema),
+    defaultValues: {
+      name: "",
+      cpf: "",
+      date: "",
+      vaccineName: "",
+      interval: "",
+      intervalCustom: "",
+      category: "",
+      dose: "",
+      observations: "",
+    },
   });
 
+  const registerWithMask = useHookFormMask(register);
+
   const onSubmit = async (data: AdminData): Promise<void> => {
+    console.log("Dados do formulário:", data);
     try {
-      const response = await api.post("/admin/vaccines", data);
+      const response = await api.post("/admin/vaccineCard", data);
       if (response.status === 201) {
         toast.success("Vacina registrada com sucesso!");
       } else {
@@ -53,75 +70,197 @@ export default function AdminForm() {
           </CardTitle>
         </CardHeader>
         <CardContent className="space-y-4">
-          {/* Nome da Vacina */}
-          <div>
-            <Label htmlFor="vaccineName">Nome da Vacina:</Label>
-            <Select {...register("vaccineName")}>
-              <SelectTrigger>
-                <SelectValue placeholder="Selecione a vacina" />
-              </SelectTrigger>
-              <SelectContent>
-                <SelectItem value="dupla-adulto">Dupla Adulto</SelectItem>
-                <SelectItem value="hepatite-b">Hepatite B</SelectItem>
-                <SelectItem value="febre-amarela">Febre Amarela</SelectItem>
-                <SelectItem value="triplice-viral">Tríplice Viral</SelectItem>
-                <SelectItem value="covid-19">COVID-19</SelectItem>
-                <SelectItem value="influenza">Influenza</SelectItem>
-                <SelectItem value="hpv">HPV</SelectItem>
-                <SelectItem value="varicela">Varicela</SelectItem>
-                <SelectItem value="meningococica-acwy">
-                  Meningocócica ACWY
-                </SelectItem>
-                <SelectItem value="pneumococica-23-valente">
-                  Pneumocócica 23-valente
-                </SelectItem>
-              </SelectContent>
-            </Select>
+          {/* Input Nome Paciente */}
+          <div className="w-full">
+            <Label htmlFor="name" className="mb-2">
+              Nome do Paciente:
+            </Label>
+            <Input id="name" type="text" {...register("name")} />
             <p className="text-red-500 mt-1 text-xs">
-              <ErrorMessage errors={errors} name="vaccineName" />
+              <ErrorMessage errors={errors} name="name" />
             </p>
           </div>
 
-          {/* Categoria */}
-          <div>
-            <Label htmlFor="category">Categoria:</Label>
-            <Select {...register("category")}>
-              <SelectTrigger>
-                <SelectValue placeholder="Selecione o público-alvo" />
-              </SelectTrigger>
-              <SelectContent>
-                <SelectItem value="adultos">Adultos</SelectItem>
-                <SelectItem value="gestantes">Gestantes</SelectItem>
-                <SelectItem value="infantis">Infantis</SelectItem>
-              </SelectContent>
-            </Select>
-            <p className="text-red-500 mt-1 text-xs">
-              <ErrorMessage errors={errors} name="category" />
-            </p>
+          {/* Input CPF e Data */}
+          <div className="grid grid-cols-2 gap-2 mb-2">
+            <div>
+              <Label htmlFor="cpf" className="mb-2">
+                CPF do Paciente:
+              </Label>
+              <Input
+                id="cpf"
+                type="text"
+                {...registerWithMask("cpf", "999.999.999-99")}
+              />
+              <p className="text-red-500 mt-1 text-xs">
+                <ErrorMessage errors={errors} name="cpf" />
+              </p>
+            </div>
+
+            <div>
+              <Label htmlFor="date" className="mb-2">
+                Data de Aplicação:
+              </Label>
+              <Input
+                id="date"
+                type="text"
+                {...registerWithMask("date", "99/99/9999")}
+              />
+              <p className="text-red-500 mt-1 text-xs">
+                <ErrorMessage errors={errors} name="date" />
+              </p>
+            </div>
           </div>
 
-          {/* Doses */}
-          <div>
-            <Label htmlFor="doses">Dosagem:</Label>
-            <Select {...register("doses")}>
-              <SelectTrigger>
-                <SelectValue placeholder="Selecione a dose" />
-              </SelectTrigger>
-              <SelectContent>
-                <SelectItem value="primeira">Primeira Dose</SelectItem>
-                <SelectItem value="segunda">Segunda Dose</SelectItem>
-                <SelectItem value="terceira">Terceira Dose</SelectItem>
-                <SelectItem value="reforco">Reforço</SelectItem>
-              </SelectContent>
-            </Select>
-            <p className="text-red-500 mt-1 text-xs">
-              <ErrorMessage errors={errors} name="doses" />
-            </p>
+          {/* Nome Vacine e Intervalo */}
+          <div className="md:grid md:grid-cols-2 mb-2 flex flex-col gap-2">
+            {/* Nome da Vacina */}
+            <div>
+              <Label htmlFor="vaccineName" className="mb-2">
+                Nome da Vacina:
+              </Label>
+              <Controller
+                name="vaccineName"
+                control={control}
+                render={({ field }) => (
+                  <Select onValueChange={field.onChange} value={field.value}>
+                    <SelectTrigger>
+                      <SelectValue placeholder="Selecione a vacina" />
+                    </SelectTrigger>
+                    <SelectContent>
+                      <SelectItem value="dupla-adulto">Dupla Adulto</SelectItem>
+                      <SelectItem value="hepatite-b">Hepatite B</SelectItem>
+                      <SelectItem value="febre-amarela">
+                        Febre Amarela
+                      </SelectItem>
+                      <SelectItem value="triplice-viral">
+                        Tríplice Viral
+                      </SelectItem>
+                      <SelectItem value="covid-19">COVID-19</SelectItem>
+                      <SelectItem value="influenza">Influenza</SelectItem>
+                      <SelectItem value="hpv">HPV</SelectItem>
+                      <SelectItem value="varicela">Varicela</SelectItem>
+                      <SelectItem value="meningococica-acwy">
+                        Meningocócica ACWY
+                      </SelectItem>
+                      <SelectItem value="pneumococica-23-valente">
+                        Pneumocócica 23-valente
+                      </SelectItem>
+                    </SelectContent>
+                  </Select>
+                )}
+              />
+              <p className="text-red-500 mt-1 text-xs">
+                <ErrorMessage errors={errors} name="vaccineName" />
+              </p>
+            </div>
+
+            {/* Intervalo */}
+            <div>
+              <Label htmlFor="interval" className="mb-2">
+                Intervalo:
+              </Label>
+
+              <Controller
+                name="interval"
+                control={control}
+                render={({ field }) => (
+                  <div>
+                    <Select onValueChange={field.onChange} value={field.value}>
+                      <SelectTrigger>
+                        <SelectValue placeholder="Selecione o intervalo" />
+                      </SelectTrigger>
+                      <SelectContent>
+                        <SelectItem value="30">30 dias</SelectItem>
+                        <SelectItem value="60">60 dias</SelectItem>
+                        <SelectItem value="90">90 dias</SelectItem>
+                        <SelectItem value="180">6 meses</SelectItem>
+                        <SelectItem value="365">1 ano</SelectItem>
+                        <SelectItem value="other">Outro</SelectItem>
+                      </SelectContent>
+                    </Select>
+
+                    {/* quando escolher "Outro", exibe um input utilizável */}
+                    {field.value === "other" && (
+                      <div className="mt-2">
+                        <Input
+                          placeholder="Informe o intervalo (dias / descrição)"
+                          {...register("intervalCustom")}
+                        />
+                      </div>
+                    )}
+                  </div>
+                )}
+              />
+              <p className="text-red-500 mt-1 text-xs">
+                <ErrorMessage errors={errors} name="category" />
+              </p>
+            </div>
+          </div>
+
+          {/* Dosagem e Categoria*/}
+          <div className="md:grid md:grid-cols-2 mb-2 flex flex-col gap-2">
+            {/* Doses */}
+            <div>
+              <Label htmlFor="dose" className="mb-2">
+                Dosagem:
+              </Label>
+
+              <Controller
+                name="dose"
+                control={control}
+                render={({ field }) => (
+                  <Select onValueChange={field.onChange} value={field.value}>
+                    <SelectTrigger>
+                      <SelectValue placeholder="Selecione a dose" />
+                    </SelectTrigger>
+                    <SelectContent>
+                      <SelectItem value="primeira">Primeira Dose</SelectItem>
+                      <SelectItem value="segunda">Segunda Dose</SelectItem>
+                      <SelectItem value="terceira">Terceira Dose</SelectItem>
+                      <SelectItem value="reforco">Reforço</SelectItem>
+                    </SelectContent>
+                  </Select>
+                )}
+              />
+              <p className="text-red-500 mt-1 text-xs">
+                <ErrorMessage errors={errors} name="dose" />
+              </p>
+            </div>
+
+            {/* Categoria */}
+            <div>
+              <Label htmlFor="category" className="mb-2">
+                Categoria:
+              </Label>
+
+              <Controller
+                name="category"
+                control={control}
+                render={({ field }) => (
+                  <Select onValueChange={field.onChange} value={field.value}>
+                    <SelectTrigger>
+                      <SelectValue placeholder="Selecione o público-alvo" />
+                    </SelectTrigger>
+                    <SelectContent>
+                      <SelectItem value="adulto">Adulto</SelectItem>
+                      <SelectItem value="gestante">Gestante</SelectItem>
+                      <SelectItem value="infantil">Infantil</SelectItem>
+                    </SelectContent>
+                  </Select>
+                )}
+              />
+              <p className="text-red-500 mt-1 text-xs">
+                <ErrorMessage errors={errors} name="category" />
+              </p>
+            </div>
           </div>
 
           {/* Observações */}
           <div>
-            <Label htmlFor="observations">Observações:</Label>
+            <Label htmlFor="observations" className="mb-2">
+              Observações:
+            </Label>
             <Textarea
               placeholder="Ex: Aplicar apenas em caso de surto"
               {...register("observations")}
@@ -132,13 +271,17 @@ export default function AdminForm() {
           </div>
 
           <Button
-          type="submit"
-          disabled={isSubmitting}
-          className="bg-green-500 hover:bg-green-600 cursor-pointer w-full text-md
+            type="submit"
+            disabled={isSubmitting}
+            className="bg-green-500 hover:bg-green-600 cursor-pointer w-full text-md
           font-bold rounded-xl shadow-lg transition-all"
-        >
-          {isSubmitting ? <Loader className="animated-spin" /> : "Registrar Vacina"}
-        </Button>
+          >
+            {isSubmitting ? (
+              <Loader className="animated-spin" />
+            ) : (
+              "Registrar Vacina"
+            )}
+          </Button>
         </CardContent>
       </Card>
     </form>
--- frontend/src/app/_validators/admin-validator.ts
+++ frontend/src/app/_validators/admin-validator.ts
@@ -1,25 +1,75 @@
 import { z } from "zod";
 
 export const adminSchema = z.object({
-  vaccineName: z
+  name: z
     .string()
-    .min(1, "O nome da vacina é obrigatório")
-    .max(100),
+    .min(1, "O nome precisa ser preenchido")
+    .max(255, "O nome deve ter no máximo 255 caracteres"),
 
-  category: z
+  cpf: z
     .string()
-    .min(1, "A categoria é obrigatória")
-    .max(50),
+    .min(1, "O CPF precisa ser preenchido")
+    .regex(/^\d{3}\.\d{3}\.\d{3}-\d{2}$/, "CPF inválido"),
 
-  doses: z
+  date: z
     .string()
-    .min(1, "A dose é obrigatória")
-    .max(50),
+    .min(1, "A data de aplicação é obrigatória")
+    .regex(/^\d{2}\/\d{2}\/\d{4}$/, "Formato de data deve ser DD/MM/YYYY")
+    .refine(
+      (val) => {
+        const [dayStr, monthStr, yearStr] = val.split("/");
+        const day = Number(dayStr);
+        const month = Number(monthStr);
+        const year = Number(yearStr);
 
+        // checa números válidos
+        if (
+          !Number.isInteger(day) ||
+          !Number.isInteger(month) ||
+          !Number.isInteger(year)
+        ) {
+          return false;
+        }
 
-    observations: z
+        // cria data e valida componentes (evita 31/02, etc.)
+        const d = new Date(year, month - 1, day);
+        return (
+          d.getFullYear() === year &&
+          d.getMonth() === month - 1 &&
+          d.getDate() === day
+        );
+      },
+      { message: "Data inválida" }
+    )
+    .refine(
+      (val) => {
+        const [dayStr, monthStr, yearStr] = val.split("/");
+        const day = Number(dayStr);
+        const month = Number(monthStr) - 1;
+        const year = Number(yearStr);
+        const d = new Date(year, month, day);
+
+        const now = new Date();
+        now.setHours(23, 59, 59, 999);
+        return d <= now;
+      },
+      { message: "A data não pode ser no futuro" }
+    ),
+
+  vaccineName: z.string().min(1, "O nome da vacina é obrigatório").max(100),
+
+  interval: z.string().min(1, "O intervalo entre doses é obrigatório").max(50),
+
+  intervalCustom: z.string().max(100, "Máximo 100 caracteres").optional(),
+
+  category: z.string().min(1, "A categoria é obrigatória").max(50),
+
+  dose: z.string().min(1, "A dose é obrigatória").max(50),
+
+  observations: z
     .string()
-    .max(500, "As observações devem ter no máximo 500 caracteres"),
+    .max(500, "As observações devem ter no máximo 500 caracteres")
+    .optional(),
 });
 
 export type AdminData = z.infer<typeof adminSchema>;
